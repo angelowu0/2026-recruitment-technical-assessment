981827a0ef5eea1c131af0581edb878de9132a42
Angelo Wu <angeloWu@student.unsw.edu.au>
2026-02-13 18:10:03 +1100

    wrote up report

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -43,7 +43,35 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
+	"""
+	parses recipeName in accordance to requirements listed out in readme:
+	- all hyphens '-' and underlines '_' are replaced with whitespace ' '
+	- all characters must be alphabetical or whitespace
+	- no leading, trailing or multiple whitespaces
+	- every word must have its first character capitalised and other letters
+	  in lower case
+	- must have > 0 characters once parsed
+
+	:param recipeName: string of the recipe name
+	:return: None if parsed string is empty, the parsed string otherwise
+	"""
+
+	# Replaces '-' and '_' with whitespace
+	parsed_recipe_name = recipeName.translate(str.maketrans("-_", "  "))
+
+	# Removes multiple, leading and trailing whitespaces
+	parsed_recipe_name = " ".join(parsed_recipe_name.split())
+
+	# Removes all non-alphabetical/whitespace characters
+	regex = re.compile("[^a-zA-Z ]")
+	parsed_recipe_name = regex.sub("", parsed_recipe_name)
+
+	# Capitalises the first letter of each word, and makes every other letter lower case
+	parsed_recipe_name = parsed_recipe_name.title()
+
+	if len(parsed_recipe_name) == 0:
+		return None
+
 	return recipeName
 
 
@@ -51,16 +79,186 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	"""
+	Endpoint for create_entry, calls add_entry to validate and add to cookbook
+
+	"""
+	try:
+		data = request.get_json()
+		entry_type = data.get("type", "")
+		name = data.get("name", "")
+		required_items = data.get("requiredItems", "")
+
+		cook_time = data.get("cookTime", "")
+
+		add_entry(entry_type, name, required_items, cook_time)
+
+		return jsonify({}), 200
+
+	except ValueError as e:
+		return str(e), 400
+
+
+
+def add_entry(entry_type: str, name: str, required_items: List[Dict[str, Union[str, int]]], cook_time: str) -> None:
+	"""
+	Validates and adds an entry to the cookbook
+
+	:param entry_type: string for the type of entry it is
+	:param name: name of the entry
+	:param required_items: the items required for entry, given as a list of dictionaries
+	:param cook_time: string for the time it takes to cook, as if entry is recipe, may be empty string
+	:return: string of error message (or NO_ERROR, ('')) if none)
+	"""
+	valid_entry(entry_type, name, required_items, cook_time)
+
+	if entry_type == "recipe":
+		print(required_items, "HEREEEEEE", flush=True)
+
+		required_items_list = [RequiredItem(item["name"], item["quantity"]) for item in required_items]
+		cookbook.append(Recipe(name, required_items_list))
+
+	if entry_type == "ingredient":
+		cookbook.append(Ingredient(name, int(cook_time)))
+
+
+def valid_entry(entry_type: str, name: str, required_items: List[Dict[str, Union[str, int]]], cook_time: str) -> None:
+	"""
+	Validates if an entry is valid
+
+	:param entry_type: type of entry
+	:param name: name of entry
+	:param required_items: list of dictionaries of entry
+	:param cook_time: string for time to cook
+	:return:Error message
+	"""
+	if entry_type == "recipe" and not no_duplicate_names_in_required_items(required_items):
+		raise ValueError("Invalid Required Items: Duplicate Names in Required Items")
+
+	elif entry_type == "ingredient" and int(cook_time) < 0:
+		raise ValueError("Invalid Cook Time: Cook Time Less Than 0")
+
+	elif entry_type not in ("recipe", "ingredient"):
+		raise ValueError("Invalid Type: Type Must Be Recipe or Ingredient")
+
+	# Checks if a name already exists in the cookbook
+	elif any(entry.name == name for entry in cookbook):
+		raise ValueError("Invalid Name: Name Already Exists")
+
+
+def no_duplicate_names_in_required_items(required_items: List[Dict[str, Union[str, int]]]) -> bool:
+	"""
+	Checks if required items has two items with the same name
+	:param required_items: List of dictionaries which represent the items
+	:return: Boolean for whether duplicates exist
+	"""
+	unique_items = set()
+
+	# Adds items into unique_items set until either reaches end, or finds item already in the set
+	for i in required_items:
+		if i["name"] not in unique_items:
+			unique_items.add(i["name"])
+
+		else:
+			return False
+
+	return True
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	try:
+		name = request.args.get("name")
+		recipe_summary = create_summary(name)
+
+		return jsonify(recipe_summary), 200
+	except ValueError as e:
+		return str(e), 400
+	except KeyError as e:
+
+		return str(e), 400
+
+
+def create_summary(name: str) -> Dict[str, Union[str, int, List[Dict[str, Union[str, int]]]]]:
+	"""
+	Creates a summary for a recipe
+	:param name: Name of recipe
+	:return: Summary in format given in readme
+	"""
+	entry = find_cookbook_entry(name)
+
+	if type(entry) == Ingredient:
+		raise ValueError("Invalid Name: Name refers to an Ingredient")
+
+	recipe_summary = do_recipe_simplify(entry, 1)
+
+	return {
+		'name': name,
+		'cook_time': recipe_summary["cook_time"],
+		'ingredients': [
+			{'name': item,
+			 'quantity': recipe_summary["ingredients"][item]} for item in recipe_summary["ingredients"]
+		]
+	}
+
+
+def do_recipe_simplify(entry: CookbookEntry, quantity: int) -> Dict[str, Union[int, Dict[str, int]]]:
+	"""
+	Recursively simplifies a recipe into its ingredients
+	:param entry: CookbookEntry of any type
+	:param quantity: Quantity of entry's base ingredients to be multiplied by
+	:return: name, cook time and ingredients dictionary
+	"""
+
+	# Base case, entry is an ingredient
+	if type(entry) == Ingredient:
+		cookbook_entry = find_cookbook_entry(entry.name)
+
+		if type(cookbook_entry) != Ingredient:
+			raise TypeError(f"Invalid Type: Expected Type Ingredient but got {type(cookbook_entry)}")
+
+		return {
+			'cook_time': getattr(cookbook_entry, "cook_time") * quantity,
+			'ingredients': {cookbook_entry.name: quantity}
+		}
+
+	ingredients: Dict[str, int] = {}
+	cook_time = 0
+
+	# Goes through every item in entry's required items and adds its ingredients and cooktime
+	for item in getattr(entry, "required_items"):
+		cookbook_entry = find_cookbook_entry(item.name)
+		temp_summary = do_recipe_simplify(cookbook_entry, item.quantity)
+		cook_time += temp_summary["cook_time"]
+		combine_required_item_dicts(ingredients, temp_summary["ingredients"])
+
+	return {
+		'cook_time': cook_time * quantity,
+		'ingredients': {ingredient: ingredients[ingredient] * quantity for ingredient in ingredients}
+	}
+
+def combine_required_item_dicts(dict_a: Dict[str, int], dict_b: Dict[str, int]) -> None:
+	"""
+	Combines dict a and b into dict a
+	:param dict_a: first dict
+	:param dict_b: second dict
+	"""
+	for item in dict_b:
+		dict_a[item] = dict_a.get(item, 0) + dict_b[item]
+
+
+def find_cookbook_entry(name: str) -> CookbookEntry:
+	"""
+	Finds a cookbook entry given its name
+	:param name: string of the cookbook entry name
+	:return: CookBookEntry corresponding to name
+	"""
+	for entry in cookbook:
+		if getattr(entry, "name") == name:
+			return entry
+	raise KeyError("Invalid Name, No Entry With Name {name}")
 
 
 # =============================================================================
--- platforms/matrix/report.md
+++ platforms/matrix/report.md
@@ -4,7 +4,25 @@
 > Use this document to record your progress, the problems you faced and how you solved (or avoided) them.
 > You can include images or add files in this directory if you want.
 
-<!-- TODO: Write about what you tried and your process here -->
+Followed Tuwunel guide using Caddy docker-compose file:
+
+Pulling docker image
+401 error failed to fetch oauth token-I, had to verify my email through docker
+
+Buy domain and set dns to cloudflare’s dns servers
+
+Setup cloudflare tunnel after login
+
+Ran the docker container using he provided compose file and couldn't connect client, so checked error logs for caddy and
+saw that i had an error 403 which caddy couldn't get a certificate from let-encrypt andthen saw I was rate limited, I had
+to change to DNS certification
+
+I ran into a problem where Cloudflare tunnel seems to be refused every ip address and was unable to resolve this issue:
+I tried:
+- using traefik as a reverse proxy
+- using caddy
+- not using a reverse proxy
+- adding various application routes to cloudflare tunnel
 
 ## Matrix Handle
 
